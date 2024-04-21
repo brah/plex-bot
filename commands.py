c60bfb6aabfe378c33e142c521a1fa386b1a7e03
lan-d <24949824+brah@users.noreply.github.com>
2024-04-21 11:18:59 +1000

    Create commands.py for command handling

---
+++ commands.py
@@ -0,0 +1,8 @@
+# Commands module for plex-bot
+class CommandHandler:
+    def __init__(self):
+        pass
+    
+    def handle_command(self, command):
+        # This will have the logic to handle different commands. Placeholder.
+        pass
